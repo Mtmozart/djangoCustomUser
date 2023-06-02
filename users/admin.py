2886968174ac9991a0599fc7db47dae5f3ad5c3a
TheSamuraiBr <mmsnborges@gmail.com>
2023-06-02 12:40:01 -0300

    Custom User

---
+++ users/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreateForm, CustomUserChangeForm
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreateForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações pessoais', {'fields': ('first_name', 'last_name', 'phone')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+
